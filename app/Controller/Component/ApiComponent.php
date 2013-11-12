d98b444ebb3b4c4134002bca69e67400f3a9530d
shyaken <shyaken@live.com>
2013-11-12 16:54:17 +0700

    update api

--- app/Controller/Component/ApiComponent.php
+++ app/Controller/Component/ApiComponent.php
@@ -9,13 +9,14 @@ define('METHOD_GET', 2);
 
 //define urls
 
-define('LASTFM_API_URL', '');
+define('LASTFM_API_URL', 'http://ws.audioscrobbler.com/2.0/');
 define('NHACCUATUI_API_URL','http://api.m.nhaccuatui.com/v4/api/');
 
 // define tokens
 
-define('SEARCH_ACCESS_TOKEN','');
+define('SEARCH_ACCESS_TOKEN','890A956AE870CC3711EFFEC8E4BD6CA7');
 define('GET_SONG_ACCESS_TOKEN','');
+define('LASTFM_ACCESS_TOKEN','5e69115a4f589e8992f11dac71266375');
 
 // define const
 
@@ -79,11 +80,18 @@ class ApiComponent extends Component {
         //CakeLog::write('error',$return_data);
         //CakeLog::write('error',$url);
         //CakeLog::write('error',$fields_string);
+        $data['status'] = false;
+        $data['return_data'] = "";
         if($return_data == ""){
+            $data['status'] = false;
             CakeLog::write("error","something went wrong, return data is null");
         }
-        
-        return $return_data;
+        else {
+            $data['status'] = true;
+            $data['return_data'] = $return_data;
+        }
+        $data['request'] = $fields_string;
+        return $data;
     }
 
     function searchForQuery($params){
@@ -97,8 +105,56 @@ class ApiComponent extends Component {
             'username' => '',
             'deviceinfo' => '',
             'token' => '890A956AE870CC3711EFFEC8E4BD6CA7',
-            'pageindex' => isset($params['pageindex']) ? $params['pageindex'] : DEFAULT_PAGE_INDEX,
-            'pagesize' => isset($params['pagesize']) ? $params['pagesize'] : DEFAULT_PAGE_SIZE,
+            'pageindex' => isset($params['page']) ? $params['page'] : DEFAULT_PAGE_INDEX,
+            'pagesize' => isset($params['limit']) ? $params['limit'] : DEFAULT_PAGE_SIZE,
+            );
+        $return_data = $this-> getJsonFromOtherApi($data);
+        $returnStr = $return_data['return_data'];
+        $rawData = json_decode($returnStr);
+        //CakeLog::write('error',print_r($rawData,1));
+        $status = $return_data['status'];
+        $hasData = false;
+        $continue = false;
+        $totalResult = 0;
+        if(isset($rawData->Items)){
+            $status = true;
+            if(count($rawData->Items) > 0){
+                $hasData = true;
+                $totalResult = $rawData->TotalRecords;
+            } else {
+                $hasData = false;
+            }
+            if(isset($rawData->GetMore)){
+                $continue = $rawData->GetMore;
+            } else{
+                $continue = 'no';
+            }
+        } else {
+            $status = false;
+            $hasData = false;
+        }
+        $returnData = array(
+            'status' => $status,
+            'hasData' => $hasData,
+            'continue' => $continue,
+            'totalResult' => $totalResult
+            );
+        if($hasData){
+            $returnData['data'] = $rawData->Items;
+        }
+        $returnData['request'] = $return_data['request'];
+        return $returnData;
+    }
+
+    function getSongInfoLastfm($params){
+        $data['method'] = METHOD_GET;
+        $data['url'] = LASTFM_API_URL;
+        $data['params'] = array(
+            'method' => 'track.getInfo',
+            'api_key' => LASTFM_ACCESS_TOKEN,
+            'artist' => $params['artist'],
+            'format' => 'json',
+            'track' => $params['name'],
             );
         $returnStr = $this-> getJsonFromOtherApi($data);
         $rawData = json_decode($returnStr);
